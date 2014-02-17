69f8d81034c29253ceb3858cfe1960e2932f4802
Raimund Schramm <raimund.schramm@gmx.net>
2014-02-17 18:19:57 +0100

    removes secret_token from repository and adds file to .gitignore

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Gemoria::Application.config.secret_token = '652b9477d0873e4f8c7df6edabed3794d72ea06492926722ce9a51701f46ab89616ad8aa34fea75ef19175938d0333a98bbef653cf8e93105530036f06c1e08a'
