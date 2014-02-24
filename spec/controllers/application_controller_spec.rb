af3c29d2759ad2849b73000cac28621f61e0e912
Raimund Schramm <raimund.schramm@gmx.net>
2014-02-24 15:36:41 +0100

    updates application_controller to implement authorization

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe ApplicationController do
+
+  controller do
+    def index
+    end
+  end
+
+  describe 'exceptions' do
+    let(:user) { build :admin }
+
+    it 'handles authorizable but Not-authenticated' do
+      current_permission = Permission.new(user)
+      controller.stub(:current_user).and_return nil
+      controller.stub(:current_permission).and_return current_permission
+
+      get :index
+      expect(response).to redirect_to login_path
+      expect(flash[:alert]).to eq 'You are not authenticated to view this page. Please log in or sign up at first.'
+    end
+
+    it 'handles Not-authorized but authenticatable' do
+      current_permission = Permission.new(user)
+      controller.stub(:current_user).and_return user
+      controller.stub(:current_permission).and_return current_permission
+      current_permission.stub(:allow?).and_return false
+
+      get :index
+      expect(response).to redirect_to root_path
+      expect(flash[:alert]).to eq 'Not authorized'
+    end
+  end
+
+  describe 'delegates and helpers' do
+    it 'delegates allow to current_permission' do
+      expect(controller).to respond_to :allow?
+    end
+  end
+
+  describe 'private methods' do
+    it 'sets the current_user' do
+      get :index
+      expect(controller.send(:current_user)).to be_nil
+    end
+
+    it 'sets the current_permission' do
+      get :index
+      expect(controller.send(:current_user)).to be_nil
+    end
+  end
+end
