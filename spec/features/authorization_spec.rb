04c323c0782258c76590d41c2d4f9f2ea4d0709e
Raimund Schramm <raimund.schramm@gmx.net>
2014-02-24 16:05:28 +0100

    adds prototype authorization feature spec; TODO decide if necessary and DRY if model-based permission approach is used

---
+++ spec/features/authorization_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+feature 'authorization' do
+
+  scenario 'As a new user When I sign up I am a non-admin user' do
+    visit '/signup'
+    fill_in 'Name',                  with: 'New user'
+    fill_in 'Password',              with: 'secret'
+    fill_in 'Password confirmation', with: 'secret'
+    click_on 'Sign up'
+    expect(User.count).to eq 1
+    expect(User.last.admin?).to be_false
+    expect(current_path).to eq '/'
+  end
+
+  scenario 'As a guest (no user) I am restricted to certain pages' do
+    visit administration_path
+    expect(page).to have_content 'Not authorized'
+  end
+
+  scenario 'As an admin I can request every page' do
+    login create(:admin)
+    visit administration_path
+    expect(page).not_to have_content 'Not authorized'
+  end
+
+  scenario 'As a member (non-admin) I am restricted to certain pages' do
+    login create(:non_admin)
+    visit administration_path
+    expect(page).to have_content 'Not authorized'
+  end
+end
