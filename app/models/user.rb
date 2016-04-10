82d3fb1268d35253c2ee7c069019d5d49833d6a4
Raimund Schramm <raimund.schramm@gmx.net>
2016-04-10 17:35:45 +0200

    updates User model to include methods for determining Role of current user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,14 @@ class User < ActiveRecord::Base
     ownerships.map(&:project_id).include?(project.id) ? true : false
   end
 
+  def has_scrum_role?(project)
+    ownerships.where(project_id: project.id).first.role.name == 'Developer' ? true : false
+  end
+
+  def includes_scrum_role?(project, role)
+    project.ownerships.pluck(:role_id).include?(role.id) ? true : false
+  end
+
   def send_password_reset
     generate_token(:password_reset_token)
     self.password_reset_sent_at = Time.zone.now
