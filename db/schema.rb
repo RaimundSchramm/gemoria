616f27e21f95400d9014bb83c89b23865c876513
Raimund Schramm <raimund.schramm@gmx.net>
2016-04-26 16:59:11 +0200

    adds migration for adding number column to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409143926) do
+ActiveRecord::Schema.define(version: 20160426141512) do
 
   create_table "acceptance_tests", force: :cascade do |t|
     t.integer  "userstory_id"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20160409143926) do
   add_index "acceptance_tests", ["userstory_id"], name: "index_acceptance_tests_on_userstory_id"
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -40,13 +40,13 @@ ActiveRecord::Schema.define(version: 20160409143926) do
   add_index "ownerships", ["role_id"], name: "index_ownerships_on_role_id"
 
   create_table "projects", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "roles", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -59,36 +59,37 @@ ActiveRecord::Schema.define(version: 20160409143926) do
   end
 
   create_table "tasks", force: :cascade do |t|
-    t.string   "name",         limit: 255
+    t.string   "name"
     t.boolean  "complete"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "userstory_id"
+    t.integer  "number"
   end
 
   add_index "tasks", ["userstory_id"], name: "index_tasks_on_userstory_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                   limit: 255
-    t.string   "password_digest",        limit: 255
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin"
-    t.string   "password_reset_token",   limit: 255
+    t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
-    t.string   "email",                  limit: 255
+    t.string   "email"
   end
 
   create_table "userstories", force: :cascade do |t|
-    t.string   "name",        limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "complete"
     t.integer  "project_id"
-    t.string   "status",      limit: 255
+    t.string   "status"
     t.text     "description"
     t.integer  "category_id"
-    t.string   "position",    limit: 255
+    t.string   "position"
     t.integer  "sprint_id"
   end
 
